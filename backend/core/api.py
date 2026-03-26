ef7952daa2664f1a8807934136d4b3b7477eb0e8
Paulo <paulo.gabriel1019@gmail.com>
2026-03-26 10:59:43 -0300

    notas: atualizando backend/core/api.py. versão 3.0

--- backend/core/api.py
+++ backend/core/api.py
@@ -1,9 +1,13 @@
 from ninja import NinjaAPI
 from engine.api import router as engine_router
-from telemetry.api import router as telemetry_router # Se já criou o arquivo lá
+from telemetry.api import router as telemetry_router
 
-api = NinjaAPI(title="Soares Gomes OS API")
+api = NinjaAPI(
+    title="Soares Gomes OS - API",
+    version="1.0.0",
+    description="Engine de controle do portfólio técnico de Paulo Gabriel"
+)
 
-# Aqui você "monta" as peças do sistema
-api.add_router("/projects", engine_router)
+# Acoplamento dos módulos
+api.add_router("/cases", engine_router)
 api.add_router("/telemetry", telemetry_router)
