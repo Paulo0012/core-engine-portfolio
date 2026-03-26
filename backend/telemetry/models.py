fbba79c923b7d7cb48d8fdba06e474b93111c271
Paulo <paulo.gabriel1019@gmail.com>
2026-03-26 10:58:28 -0300

    notas: subindo backend/telemetry/models.py

--- backend/telemetry/models.py
+++ backend/telemetry/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class DeviceStatus(models.Model):
+    device_name = models.CharField(max_length=100) # Ex: "Raspberry-Pico-W"
+    cpu_usage = models.FloatField()
+    memory_usage = models.FloatField()
+    is_online = models.BooleanField(default=True)
+    last_ping = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.device_name} - {self.last_ping}"
