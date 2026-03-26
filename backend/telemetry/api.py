f5892387972dbccb01d09eaff9323ec83deb9b6a
Paulo <paulo.gabriel1019@gmail.com>
2026-03-26 10:59:08 -0300

    notas: atualizando backend/telemetry/api.py. versão 2.0

--- backend/telemetry/api.py
+++ backend/telemetry/api.py
@@ -1,13 +1,22 @@
-from ninja import Router
-import datetime
+from ninja import Router, Schema
+from .models import DeviceStatus
+import random
 
 router = Router()
 
-@router.get("/status")
-def get_system_status(request):
+class TelemetryOut(Schema):
+    device_name: str
+    cpu_usage: float
+    memory_usage: float
+    is_online: bool
+
+@router.get("/live", response=TelemetryOut)
+def get_live_metrics(request):
+    """Retorna dados reais ou simulados para o Dashboard do Front"""
+    # No futuro, aqui você leria do banco ou de um Broker MQTT
     return {
-        "status": "online",
-        "server_time": datetime.datetime.now(),
-        "active_nodes": 1, # Aqui simularíamos seu hardware
-        "load": "0.45"
+        "device_name": "Core-Engine-UFMA",
+        "cpu_usage": round(random.uniform(15.0, 65.0), 2),
+        "memory_usage": round(random.uniform(200.0, 800.0), 2),
+        "is_online": True
     }
