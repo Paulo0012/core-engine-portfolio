54e849c78882d697e9b3ec911c91a98c51f55379
Paulo <paulo.gabriel1019@gmail.com>
2026-03-26 11:08:14 -0300

    notaas: subindo backend/core/auth, atualziando os imports

--- backend/core/auth.py
+++ backend/core/auth.py
@@ -1,20 +1,24 @@
-from ninja.security import HttpBearer
-from django.contrib.auth.models import User
-import jwt # pip install pyjwt
-from django.conf import settings
+import jwt
 from datetime import datetime, timedelta
+from django.conf import settings
+from django.contrib.auth.models import User
+from ninja.security import HttpBearer
 
 class JWTAuth(HttpBearer):
     def authenticate(self, request, token):
         try:
+            # Decodifica o token usando a SECRET_KEY do Django
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             user = User.objects.get(username=payload["username"])
             return user
         except Exception:
             return None
 
+# ESTA LINHA É A QUE ESTÁ FALTANDO:
+auth_bearer = JWTAuth()
+
 def create_token(user):
-    """Gera o token para o Frontend armazenar no localStorage"""
+    """Gera o token para o Frontend"""
     payload = {
         "username": user.username,
         "exp": datetime.utcnow() + timedelta(days=7),
