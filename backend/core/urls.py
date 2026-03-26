781c059ad07c362df2c5f90d12ee27b35021bf33
Paulo <paulo.gabriel1019@gmail.com>
2026-03-26 11:12:52 -0300

    notaas: subindo backend/core/urls.py, versão 2.0

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .api import api
+from .api import api # Importa a instância unificada da API
 
 urlpatterns = [
-    path('admin/', admin.site.core),
-    path("api/v1/", api.urls),
+    # O erro estava aqui. O correto é admin.site.urls
+    path('admin/', admin.site.urls), 
+    
+    # Nossa API Ninja
+    path("api/v1/", api.urls), 
 ]
