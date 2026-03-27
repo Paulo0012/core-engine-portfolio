8974b28f789b0066ceff93957cdd08779e2e9daa
Paulo0012 <paulo.gabriel1019@gmail.com>
2026-03-27 20:31:18 -0300

    Notas: atualizando frontend/service/api.js, INTERCEPTOR: Se houver um token no localStorage, ele injeta no Header

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-// Instância centralizada do motor de conexão
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
-  timeout: 5000, // Evita que o sistema trave se o Django estiver offline
+  baseURL: 'http://localhost:8000/api',
 });
 
-// Interceptor: Injeta o Token JWT em todas as requisições para o Dashboard
+// INTERCEPTOR: Se houver um token no localStorage, ele injeta no Header
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
