7e63929156099e687077f38db315881a61a5f1ef
Paulo0012 <paulo.gabriel1019@gmail.com>
2026-03-26 22:35:28 -0300

    notas: criando arquivo Frontend/src/App.tsx, importando react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import Dashboard from './pages/Dashboard';
 import EngineeringBio from './pages/EngineeringBio';
+import ProjectDetails from './pages/ProjectDetails';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* O MainLayout contém a Sidebar, Header e SystemLogs fixos */}
         <Route path="/" element={<MainLayout />}>
+          {/* Rota Principal: Grid de Projetos */}
           <Route index element={<Dashboard />} />
+          
+          {/* Rota de Biografia: Sua Identidade Visual e Acadêmica */}
           <Route path="bio" element={<EngineeringBio />} />
-          {/* Rotas futuras como Detalhes de Projeto aqui */}
+          
+          {/* Rota de Detalhes: Deep Dive Técnico (Hardware/Software) */}
+          <Route path="project/:id" element={<ProjectDetails />} />
         </Route>
       </Routes>
     </Router>
