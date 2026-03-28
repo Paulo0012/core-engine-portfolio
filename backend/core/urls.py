75d21c9ca5f8c78a0167537a8b7a409e67f9f1b1
Paulo0012 <paulo.gabriel1019@gmail.com>
2026-03-27 21:22:28 -0300

    Notas: atualizando backend/core/urls, adcioando urlpatterns

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from .api import api # Importa a instância unificada da API
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # O erro estava aqui. O correto é admin.site.urls
@@ -9,3 +11,6 @@ urlpatterns = [
     # Nossa API Ninja
     path("api/v1/", api.urls), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
